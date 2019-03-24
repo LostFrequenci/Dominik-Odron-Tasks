ccb798476f0945b212c03c9103a67c384e653cf4
LostFrequenci <dominik.odron@gmail.com>
2019-03-24 14:16:42 +0100

    Moduł 24 vol 4

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -11,14 +12,23 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message",message);
-        context.setVariable("tasks_url","http://localhost:8888/tasks_frontend");
+        context.setVariable("tasks_url","https://lostfrequenci.github.io/");
         context.setVariable("button","Visit website");
+        context.setVariable("preview_message","New trello card created");
+        context.setVariable("goodbye_message","Have a nice day !");
+        context.setVariable("admin_name",adminConfig.getAdminName());
+        context.setVariable("company_name",adminConfig.getCompanyName());
+        context.setVariable("company_goal",adminConfig.getCompanyGoal());
+        context.setVariable("company_email",adminConfig.getCompanyEmail());
         return templateEngine.process("created-trello-card-mail",context);
     }
 }
